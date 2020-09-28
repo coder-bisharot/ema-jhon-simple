b35baf7b41b63ace784de2b52fe1751c70701c3c
coder-bisharot <ahamedbisharot@.com>
2020-09-28 15:38:38 +0600

    dataPash

--- src/Components/ProductDetails/ProductDetails.js
+++ src/Components/ProductDetails/ProductDetails.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-// import fakeData from '../../fakeData';
-// import Product from '../Product/Product';
+import fakeData from '../../fakeData';
+import Product from '../Product/Product';
 
 const ProductDetails = () => {
     const {productKey} = useParams();
-    
+    const product = fakeData.find (pd=>pd.key ===productKey);
     return (
         <div>
             <h1>{productKey}coming soon........</h1>
+            <Product product={product}></Product>
         </div>
     );
 };
